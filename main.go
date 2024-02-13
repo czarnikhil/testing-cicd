e8807d4578ae868dc47db4c64358fae3eb579558
Nikhil <czarnikhil27@gmail.com>
2024-02-13 15:01:50 +0545

    fef

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	fmt.Println("Server listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, worlfdd!")
+	fmt.Println("fdf")
 }
