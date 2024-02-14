df4c5be347d42a0a8de1a334c4ecb79f4a2a7e78
Nikhil <czarnikhil27@gmail.com>
2024-02-14 16:27:43 +0545

    created endpoint

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("fdf")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	fmt.Println("Server listening on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
